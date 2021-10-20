3619a7e5085d79ad28b6dd2ed54788dfaec71c09
Mr. Kiedes <akiedes@vtsd.com>
2021-10-20 13:11:24 -0400

    Boilerplate Code

---
+++ catchATurtle.py
@@ -0,0 +1,14 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+
+#-----game configuration----
+
+
+#-----initialize turtle-----
+
+
+#-----game functions--------
+
+
+#-----events----------------
