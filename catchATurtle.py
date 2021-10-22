d39753985957d6a8a8271b9a769c47a9e73d7bdf
Mr. Kiedes <akiedes@vtsd.com>
2021-10-22 11:39:58 -0400

    Friday Update

--- catchATurtle.py
+++ catchATurtle.py
@@ -1,14 +1,40 @@
+'''
+Random appearance of the shape on different parts of the screen
+The event of a shape being clicked
+The score updating
+The timer updating
+'''
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
 
 #-----game configuration----
+xMin = 0
+xMax = 112
+yMin = 0
+yMax = 324
 
+color = "red"
+shape = "triangle"
+size = 2
 
 #-----initialize turtle-----
-
+t = trtl.Turtle()
+t.shape(shape)
+t.turtlesize(size)
+t.fillcolor(color)
 
 #-----game functions--------
 
 
+
+def spot_clicked(x, y):
+    print("IT WORKED!!!!!!!!!")
+
 #-----events----------------
+t.onclick(spot_clicked)
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
